765d2465b6ee6d49916a1bc83feabd9c6d2ede6c
Enfant-de-lumiere <tantonio004@gmail.com>
2021-03-29 11:03:13 +0200

    creation partie terminer manque les images

--- projettest/src/main.c
+++ projettest/src/main.c
@@ -1,112 +1,137 @@
-/*
-    GNU/Linux et MacOS
-        > gcc main.c $(sdl2-config --cflags --libs) -o prog
-        > gcc *.c $(sdl2-config --cflags --libs) -o prog
-    Windows
-        > gcc src/main.c -o bin/prog -I include -L lib -lmingw32 -lSDL2main -lSDL2
-        > gcc src/main.c -o bin/prog -I include -L lib -lmingw32 -lSDL2main -lSDL2 -mwindows
-*/
 #include <stdio.h>
-#include <stdlib.h>
 #include <SDL.h>
+#include <SDL_image.h>
+#include <stdlib.h>
 #define Fenetre_width 1280
 #define Fenetre_height 720
-void SDL_ExitWithError(const char *message);
 
-int main(int argc, char *argv[])
+// gcc src/main.c -o bin/prog -I include -L lib -lmingw32 -lSDL2main -lSDL2 -lSDL2_image
+// bin\prog.exe
+
+
+
+
+void SDL_ExitWithError(const char *message)
 {
-    SDL_Window *window = NULL;
-    SDL_Renderer *rendu= NULL;
-    SDL_Surface *image =NULL;
-    SDL_Texture *texture=NULL;
-    SDL_Rect rectangle;
-    rectangle.x=Fenetre_width/4;
-    rectangle.y=Fenetre_height/4;
-    int droite=0;
-    int imagew=Fenetre_width/2;
-    int imageh=Fenetre_height/2;
-    SDL_bool program =SDL_TRUE;
+SDL_Log("ERREUR : %s > %s\n", message, SDL_GetError());
+SDL_Quit();
+exit(EXIT_FAILURE);
+}
 
-    //Lancement SDL
-    if(SDL_Init(SDL_INIT_VIDEO) != 0)
-        SDL_ExitWithError("Initialisation SDL");
-    //Création fenêtre et rendu
-    if(SDL_CreateWindowAndRenderer(Fenetre_width,Fenetre_height,0,&window,&rendu) != 0)
-        SDL_ExitWithError("Impossible de creer la fenetre et le rendu");
 
- if(SDL_SetRenderDrawColor(rendu,0,0,0,SDL_ALPHA_OPAQUE) !=0)
-        SDL_ExitWithError("Impossible de changer la couleur pour le rendu");
+void clean_ressources(SDL_Window *w,SDL_Renderer *r,SDL_Texture *t)
+{
+    if(t != NULL)
+        SDL_DestroyTexture(t);
+    if(r != NULL)
+        SDL_DestroyRenderer(r);
+    if(w != NULL)
+        SDL_DestroyWindow(w);
 
-    /*------------------------------------------------------------*/
-    image = SDL_LoadBMP("src/étoile.bmp");
+}
+
+void SDL_AfficherUneImage(SDL_Renderer *rendu,SDL_Surface *image,SDL_Texture *texture,SDL_Rect rectangle){
     if(image ==NULL){
-        SDL_DestroyRenderer(rendu);
-        SDL_DestroyWindow(window);
-         SDL_ExitWithError("Impossible de charger l'image");
+    clean_ressources(NULL,rendu,texture);
+    SDL_ExitWithError("Impossible de charger l'image");
     }
     texture= SDL_CreateTextureFromSurface(rendu,image);
 
     SDL_FreeSurface(image);
      if(texture ==NULL){
-        SDL_DestroyRenderer(rendu);
-        SDL_DestroyWindow(window);
-         SDL_ExitWithError("Impossible de creer la texture");
+        clean_ressources(NULL,rendu,texture);
+        SDL_ExitWithError("Impossible de creer la texture");
     }
     if(SDL_QueryTexture(texture,NULL,NULL,&rectangle.w,&rectangle.h)!=0){
-        SDL_DestroyRenderer(rendu);
-        SDL_DestroyWindow(window);
+        clean_ressources(NULL,rendu,texture);
         SDL_ExitWithError("Impossible de charger la texture");
     }
     if(SDL_RenderCopy(rendu,texture,NULL,&rectangle)!=0){
-        SDL_DestroyRenderer(rendu);
-        SDL_DestroyWindow(window);
+        clean_ressources(NULL,rendu,texture);
         SDL_ExitWithError("Impossible d'afficher la texture");
     }
-   /*   
-    if(SDL_RenderDrawPoint(rendu,100,450)!= 0)
-        SDL_ExitWithError("Impossible de dessiner un point");
-    
-    
-
-    if(SDL_RenderFillRect(rendu,&rectangle)!= 0)
-        SDL_ExitWithError("Impossible de dessiner un rectangle");
-   */
-
     SDL_RenderPresent(rendu);
-    while(program)
-    {
-        SDL_Event event;
-        while(SDL_PollEvent(&event))
-        {
-            switch(event.type)
-            {
-                case SDL_QUIT:
-                    program=SDL_FALSE;
-                    break;
-                case SDL_MOUSEBUTTONDOWN:
-                    printf("Click on %d / %d\n", event.motion.x,event.motion.y);
-                    break;
-               /* case SDL_MOUSEBUTTONUP:
-                    if(event.motion.x>imagew)
-                */default:
-                    break;
-            }
-        }
-
-    }
-
-    /*------------------------------------------------------------*/
-    SDL_DestroyTexture(texture);
-    SDL_DestroyRenderer(rendu);
-    SDL_DestroyWindow(window);
-    SDL_Quit();
-    
-    return EXIT_SUCCESS;
 }
 
-void SDL_ExitWithError(const char *message)
+
+int main(int argc, char ** argv)
 {
-    SDL_Log("ERREUR : %s > %s\n", message, SDL_GetError());
-    SDL_Quit();
-    exit(EXIT_FAILURE);
+    SDL_Window *window =NULL;
+    SDL_Renderer *rendu=NULL;
+    SDL_Surface *picture = NULL;
+    SDL_Texture *texture= NULL;
+    int i=0;
+    SDL_Rect rectangle={0,0,Fenetre_width,Fenetre_height};
+
+
+    if(SDL_Init(SDL_INIT_VIDEO)!=0)
+    {
+      SDL_ExitWithError("Initialisation SDL");
+    }
+
+    if(SDL_CreateWindowAndRenderer(Fenetre_width,Fenetre_height,0,&window,&rendu) != 0)
+        SDL_ExitWithError("Impossible de creer la fenetre et le rendu");
+
+
+    picture = IMG_Load("src/arbre.jpg");
+    SDL_AfficherUneImage( rendu, picture, texture, rectangle);
+    picture = IMG_Load("src/creer_une_partie.png");
+    SDL_Rect rectangle1={0,0,0,0};
+      SDL_AfficherUneImage( rendu, picture, texture, rectangle1);
+    picture = IMG_Load("src/rejoindre_une_partie.png");
+    SDL_Rect rectangle2={0,100,0,0};
+      SDL_AfficherUneImage( rendu, picture, texture, rectangle2);
+    picture = IMG_Load("src/options.png");
+      SDL_Rect rectangle3={0,200,0,0};
+        SDL_AfficherUneImage( rendu, picture, texture, rectangle3);
+    picture = IMG_Load("src/quitter_le_jeu.png");
+    SDL_Rect rectangle4={0,300,0,0};
+    SDL_AfficherUneImage( rendu, picture, texture, rectangle4);
+
+
+
+    SDL_bool program_launched = SDL_TRUE;
+    while(program_launched)
+    {
+      SDL_Event event;
+      while(SDL_PollEvent(&event))
+      {
+        switch(event.type)
+        {
+
+          case SDL_QUIT:
+          program_launched=SDL_FALSE;
+          break;
+
+          case SDL_MOUSEBUTTONUP:
+            //if( event.button.button == SDL_BUTTON_LEFT){                               //si le bouton de gauche est appuyé
+            printf("clic gauche sur %d/%d",event.button.x,event.button.y);
+
+              if((event.button.x > 00 && event.button.x < 100) && (event.button.y > 0 && event.button.y < 480 )){ // endroit ou il y aura le bouton pour lancer le jeu
+
+                  /* programme pour créer une partie */
+
+            }
+            if((event.button.x > 100 && event.button.x < 200) && (event.button.y > 0 && event.button.y < 480 )){ // endroit ou il y aura le bouton pour rejoindre le jeu
+                /* programme pour rejoindre une parties */
+              }
+              if((event.button.x > 200 && event.button.x < 300) && (event.button.y > 0 && event.button.y < 480 )){ // endroit ou il y aura le bouton pour rejoindre le jeu
+                  /* programme pour les options */
+
+                  }
+                if((event.button.x > 0 && event.button.x < 400) && (event.button.y > 0 && event.button.y < 480 )){  // endroit ou il y aura le bouton pour quitter le jeu
+                  program_launched = SDL_FALSE;
+
+                }
+              break;
+              default:
+              break;
+          }
+      }
+  }
+  clean_ressources(window,rendu,texture);
+  SDL_Quit();
+  return EXIT_SUCCESS;
+
+
 }
--- projettest/src/petite_fonction.h
+++ projettest/src/petite_fonction.h
@@ -4,6 +4,8 @@
 #include <SDL_image.h>
 #define Fenetre_width 1280
 #define Fenetre_height 720
+
 void clean_ressources(SDL_Window *w,SDL_Renderer *r,SDL_Texture *t);
 void SDL_AfficherUneImage(SDL_Renderer *rendu,SDL_Surface *image,SDL_Texture *texture,SDL_Rect rectangle);
 void SDL_ExitWithError(const char *message);
+void creation_partie(int * joueur, int * tour );
